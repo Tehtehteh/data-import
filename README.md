34f61b61bc2df1ebb257d5abe70cd332142e5f85
Tehtehteh <tehfvpr@gmail.com>
2020-09-29 12:19:44 +0300

    Readme.md + pravda rss parser test.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Data-import service
+Сервис предназначен для импорта разных data source (может быть чем угодно: rss feed, html-документ и прочее)
+в RMQ-очередь для последующей обработки Data-Pipeline сервисом.
+
+# Поддерживаемые data sources (обновляется)
+
+1. [Unian](https://unian.ua) 
+1. [Pravda.com.ua](https://www.pravda.com.ua/rus/rss/) 
+ 
--- tests/test_parsers/test_rss.py
+++ tests/test_parsers/test_rss.py
@@ -1,8 +1,18 @@
-from parsers import UnianRssParser
+from parsers import (
+    UnianRssParser,
+    PravdaRssParser
+)
 
 
 class TestUnianParser:
     def test_parses_correctly(self, unian_rss):
         parser = UnianRssParser(unian_rss)
+        articles = parser.parse()
+        assert len(articles) > 0
+
+
+class TestPravdaParser:
+    def test_parses_correctly(self, pravda_rss):
+        parser = PravdaRssParser(pravda_rss)
         articles = parser.parse()
         assert len(articles) > 0
